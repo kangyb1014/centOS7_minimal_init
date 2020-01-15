ec9e91bc4702d5a4ace4aaab8464f5824fcdfefd
kangyb1014 <clear1014@gmail.com>
2020-01-15 11:41:13 +0900

    php 패키지 설치하게 변경 및 phpinfo 권한 수정 스크립트 추가

--- php_install.sh
+++ php_install.sh
@@ -18,6 +18,17 @@ sudo yum-config-manager --disable remi-php54
 # activate php 7.3
 sudo yum-config-manager --enable remi-php73
 # install php 7.3
-yum -y install php-fpm
+yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql -y
 
-sed -i 's/index.html/index.html index.php' /var/httpd/conf/httpd.conf
+# httpd.conf 파일 수정하여 index.php 추가
+sed -i 's/index.html/index.php index.html/' /etc/httpd/conf/httpd.conf
+
+sed -i 's,\(\AddType application/x-gzip .gz .tgz.*\),\1\n\tAddType application/x-httpd-php .php .html .htm .inc\n\tAddType application/x-httpd-php-source .phps,g' /etc/httpd/conf/httpd.conf
+
+# document root에 index.php 추가
+cp ./scripts/index.php /var/www/html
+
+chmod 755 /var/www/html/index.php
+
+# apache 재시작
+systemctl restart httpd
